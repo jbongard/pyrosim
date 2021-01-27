fa5fc5cc9daae2f538217dec3ade0c652f4494b2
proteusInstitute <jbongard@uvm.edu>
2021-01-27 09:38:55 -0500

    First commit.

---
+++ neuralNetwork.py
@@ -0,0 +1,101 @@
+from ludobots.neuron  import NEURON
+
+from ludobots.synapse import SYNAPSE
+
+class NEURAL_NETWORK: 
+
+    def __init__(self,nndfFileName):
+
+        self.neurons = {}
+
+        self.synapses = {}
+
+        f = open(nndfFileName,"r")
+
+        for line in f.readlines():
+
+            self.Digest(line)
+
+        f.close()
+
+    def Print(self):
+
+        self.Print_Sensor_Neuron_Values()
+
+        self.Print_Hidden_Neuron_Values()
+
+        self.Print_Motor_Neuron_Values()
+
+        print("")
+
+# ---------------- Private methods --------------------------------------
+
+    def Add_Neuron_According_To(self,line):
+
+        neuron = NEURON(line)
+
+        self.neurons[ neuron.Get_Name() ] = neuron
+
+    def Add_Synapse_According_To(self,line):
+
+        synapse = SYNAPSE(line)
+
+        sourceNeuronName = synapse.Get_Source_Neuron_Name()
+
+        targetNeuronName = synapse.Get_Target_Neuron_Name()
+
+        self.synapses[sourceNeuronName , targetNeuronName] = synapse
+
+    def Digest(self,line):
+
+        if self.Line_Contains_Neuron_Definition(line):
+
+            self.Add_Neuron_According_To(line)
+
+        if self.Line_Contains_Synapse_Definition(line):
+
+            self.Add_Synapse_According_To(line)
+
+    def Line_Contains_Neuron_Definition(self,line):
+
+        return "neuron" in line
+
+    def Line_Contains_Synapse_Definition(self,line):
+
+        return "synapse" in line
+
+    def Print_Sensor_Neuron_Values(self):
+
+        print("sensor neuron values: " , end = "" )
+
+        for neuronName in sorted(self.neurons):
+
+            if self.neurons[neuronName].Is_Sensor_Neuron():
+
+                self.neurons[neuronName].Print()
+
+        print("")
+
+    def Print_Hidden_Neuron_Values(self):
+
+        print("hidden neuron values: " , end = "" )
+
+        for neuronName in sorted(self.neurons):
+
+            if self.neurons[neuronName].Is_Hidden_Neuron():
+
+                self.neurons[neuronName].Print()
+
+        print("")
+
+    def Print_Motor_Neuron_Values(self):
+
+        print("motor neuron values: " , end = "" )
+
+        for neuronName in sorted(self.neurons):
+
+            if self.neurons[neuronName].Is_Motor_Neuron():
+
+                self.neurons[neuronName].Print()
+
+        print("")
---
+++ pyrosim.py
@@ -0,0 +1,242 @@
+import pybullet as p
+
+from ludobots.nndf import NNDF
+
+from ludobots.linksdf  import LINK_SDF
+
+from ludobots.linkurdf import LINK_URDF
+
+from ludobots.model import MODEL
+
+from ludobots.sdf   import SDF
+
+from ludobots.urdf  import URDF
+
+from ludobots.joint import JOINT
+
+SDF_FILETYPE  = 0
+
+URDF_FILETYPE = 1
+
+NNDF_FILETYPE   = 2
+
+# global availableLinkIndex
+
+# global linkNamesToIndices
+
+def End():
+
+    if filetype == SDF_FILETYPE:
+
+        sdf.Save_End_Tag(f)
+
+    elif filetype == NNDF_FILETYPE:
+
+        nndf.Save_End_Tag(f)
+    else:
+        urdf.Save_End_Tag(f)
+
+    f.close()
+
+def End_Model():
+
+    model.Save_End_Tag(f)
+
+def Get_Touch_Sensor_Value_For_Link(linkName):
+
+    touchValue = -1.0
+
+    desiredLinkIndex = linkNamesToIndices[linkName]
+
+    pts = p.getContactPoints()
+
+    for pt in pts:
+
+        linkIndex = pt[4]
+
+        if ( linkIndex == desiredLinkIndex ):
+
+            touchValue = 1.0
+
+    return touchValue
+
+def Prepare_Link_Dictionary(urdfFileName):
+
+    global linkNamesToIndices
+
+    linkNamesToIndices = {}
+
+    linkIndex = -1
+
+    f = open(urdfFileName,"r")
+
+    for line in f.readlines():
+
+        if "link name" in line:
+
+            line = line.split('"')
+
+            linkName = line[1]
+
+            linkNamesToIndices[linkName] = linkIndex
+
+            linkIndex = linkIndex + 1
+
+    f.close()
+
+def Prepare_Joint_Dictionary(urdfFileName):
+
+    global jointNamesToIndices
+
+    jointNamesToIndices = {}
+
+    jointIndex = 0
+
+    f = open(urdfFileName,"r")
+
+    for line in f.readlines():
+
+        if "joint name" in line:
+
+            line = line.split('"')
+
+            jointName = line[1]
+
+            jointNamesToIndices[jointName] = jointIndex
+
+            jointIndex = jointIndex + 1
+
+    f.close()
+
+def Prepare_To_Simulate(urdfFileName):
+
+    Prepare_Link_Dictionary(urdfFileName)
+
+    Prepare_Joint_Dictionary(urdfFileName)
+
+def Send_Cube(name="default",pos=[0,0,0],size=[1,1,1]):
+
+    global availableLinkIndex
+
+    if filetype == SDF_FILETYPE:
+
+        Start_Model(name,pos)
+
+        link = LINK_SDF(name,pos,size)
+    else:
+        link = LINK_URDF(name,pos,size)
+
+    link.Save(f)
+
+    if filetype == SDF_FILETYPE:
+
+        End_Model()
+
+    linkNamesToIndices[name] = availableLinkIndex
+
+    availableLinkIndex = availableLinkIndex + 1
+
+def Send_Joint(name,parent,child,type,position):
+
+    joint = JOINT(name,parent,child,type,position)
+
+    joint.Save(f)
+
+def Send_Motor_Neuron(name,jointName):
+
+    f.write('    <neuron name = "' + str(name) + '" type = "motor"  jointName = "' + jointName + '" />\n')
+
+def Send_Sensor_Neuron(name,linkName):
+
+    f.write('    <neuron name = "' + str(name) + '" type = "sensor" linkName = "' + linkName + '" />\n')
+
+def Send_Synapse( sourceNeuronName , targetNeuronName , weight ):
+
+    f.write('    <synapse sourceNeuronName = "' + str(sourceNeuronName) + '" targetNeuronName = "' + str(targetNeuronName) + '" weight = "' + str(weight) + '" />\n')
+
+ 
+def Set_Motor_For_Joint(bodyIndex,jointName,controlMode,targetPosition,maxForce):
+
+    p.setJointMotorControl2(
+
+        bodyIndex      = bodyIndex,
+
+        jointIndex     = jointNamesToIndices[jointName],
+
+        controlMode    = controlMode,
+
+        targetPosition = targetPosition,
+
+        force          = maxForce)
+
+def Start_NeuralNetwork(filename):
+
+    global filetype
+
+    filetype = NNDF_FILETYPE
+
+    global f
+
+    f = open(filename,"w")
+
+    global nndf
+
+    nndf = NNDF()
+
+    nndf.Save_Start_Tag(f)
+
+def Start_SDF(filename):
+
+    global availableLinkIndex
+
+    availableLinkIndex = -1
+
+    global linkNamesToIndices
+
+    linkNamesToIndices = {}
+
+    global filetype
+
+    filetype = SDF_FILETYPE
+
+    global f
+ 
+    f = open(filename,"w")
+
+    global sdf
+
+    sdf = SDF()
+
+    sdf.Save_Start_Tag(f)
+
+def Start_URDF(filename):
+
+    global availableLinkIndex
+
+    availableLinkIndex = -1
+
+    global linkNamesToIndices
+
+    linkNamesToIndices = {}
+
+    global filetype
+
+    filetype = URDF_FILETYPE
+
+    global f
+
+    f = open(filename,"w")
+
+    global urdf 
+
+    urdf = URDF()
+
+    urdf.Save_Start_Tag(f)
+
+def Start_Model(modelName,pos):
+
+    global model 
+
+    model = MODEL(modelName,pos)
+
+    model.Save_Start_Tag(f)
